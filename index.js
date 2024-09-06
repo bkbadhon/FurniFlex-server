5607eef53129796a314e6780eae1ba2d2183169b
bkbadhon <chandrobadhon557@gmail.com>
2024-09-07 00:54:59 +0600

    complete

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://furniflex-49b63.web.app'],
     credentials: true, 
   };
 
